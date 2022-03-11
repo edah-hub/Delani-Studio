5130e812e44d00f0ca402ff8a8fe89418c7a7978
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-03-11 17:03:59 +0300

    adds jquery functions

--- js/main.js
+++ js/main.js
@@ -6,7 +6,44 @@ $(document).ready(function(){
     $(":focus").css("color", "white");
   });
 
-  $(document).ready(function(){
-      $(#)
-  }
-  )
+
+//   design
+
+$(document).ready(function(){
+    $("#design_img").click(function(){
+      $("#design_img").toggle('2000').hide('1000');
+      $("#design").show('2000');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design_img").slideDown('1500');
+    });
+  });
+  
+//   development
+
+$(document).ready(function(){
+    $("#development_img").click(function(){
+      $("#development_img").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideUp('2000');
+      $("#development_img").slideDown('2000');
+      $("#development_img").css(margin,5);
+    });
+  });
+
+//   product
+$(document).ready(function(){
+  $("#product_img").click(function(){
+    $("#product_img").slideDown('2000').hide('1000');
+    $("#product").show('2000');
+  });
+  $("#product").click(function(){
+    $("#product").slideUp('1500');
+    $("#product_img").slideDown('1500');
+  });
+});
+
+
