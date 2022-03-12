83ba25985b89e7ed271c2d3e1b27793f0c8a81bb
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-03-13 01:30:43 +0300

    styles the form

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // $(document).ready(function(){
 //     $(input).focus.css(color,purple);
 // });
-$(document).ready(function(){
-    $("input").focus();
-    $(":focus").css("color", "white");
-  });
+// $(document).ready(function(){
+//     $("input").focus();
+//     $(":focus").css("color", "white");
+//   });
 
 
 //   design
